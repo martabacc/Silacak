eaedaa5288888c22b115b0704388f6673661ee30
Ronauli S <deasnataly@gmail.com>
2016-10-25 12:36:51 +0700

    reinitiate project

--- application/language/english/module/citation_lang.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-$lang['cit_jumlah'] = 'Jumlah Dikutip';
-$lang['cit_tahun'] = 'Tahun';
--- application/views/errors/page_error.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<div class="row">
-	<div class="col-md-12">
-		<div class="note note-danger">
-			<h4 class="block"><?php echo $this->lang->line('error_subtitle'); ?></h4>
-			<p><?php echo $error; ?></p>
-		</div>
-	</div>
-</div>
